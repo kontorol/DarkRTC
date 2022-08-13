819ef5abf8357a0488a21a91be82222aeb973502
kontorol <60671553+kontorol@users.noreply.github.com>
2022-08-13 12:35:48 -0700

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,6 +13,9 @@
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
+# Copy Default DarkRTC Packages
+cp -rf ./DarkRTC/ openwrt/package/darkrtc
+
 # Add a feed source
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
